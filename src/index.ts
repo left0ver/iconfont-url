47ae03505ebd294be76732b3458605745d47b489
left0ver <1614674987@qq.com>
2023-01-10 19:13:49 +0800

    refactor: remove noused code

--- src/index.ts
+++ src/index.ts
@@ -45,5 +45,3 @@ export async function getOnlineUrl(projectName: string, cookie?: string) {
 }
 
 export type { FontUrl }
-
-getOnlineUrl('vue', 'ctoken=6e1Yzhl3KHCdorMPV56Pv0iB; cna=W/3aGwMnGiYCAWVQeLRp6Skp; xlly_s=1; EGG_SESS_ICONFONT=LwyPfFe9mdLtPg5kAyRM2oKTWe0dYQN45w0qN-zVhK6dp-njsVT2v8vj0y7Na1xO_qxR0Q5yn44fvFMYv4dAbFkkPN7Q2SIT7WFQ5FO2LlJVKsCyelzFFL8JjTAVNK-3jyCRMmszG1y4c25mFifA0KCTRNGsC3yO4F5BQ7TmkGklE3PTdw0HJK8iKAk8vOfcosi1out1fHvBNMvkz8FNSQ==; u=8520645; u.sig=s3SVY9PtAbD8LE5GAHj0_MUPeCU3Mgt8M0-Qb1rj7jU; isg=BHJyitcnRTdd0XkHHopIhzTEw75UA3ad0BSLjTxPiyXbzxTJJJMPrTVtvmvz1-41').then(res => console.log(res)).catch(err => console.log(err))
