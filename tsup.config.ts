e0aa4d6e0bd9b3a5019f6672a35cafd796114a8d
left0ver <1614674987@qq.com>
2023-01-10 08:56:46 +0800

    chore: generator .cjs file instead of .js file

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup'
+//  tsup config file
+export default defineConfig({
+  outExtension({ format }) {
+    // esm
+    if (format === 'esm') {
+      return {
+        js: `.mjs`
+      }
+    }
+    // global
+    if (format==='iife') {
+      return {
+        js: `.global.js`
+      }
+    }
+    // cjs
+    return {
+      js: `.cjs`,
+    }
+  }
+}) 
